bde83b1218745271ff2865d6b98f58aab53e4f2c
Pooya Parsa <pyapar@gmail.com>
2018-01-07 01:02:05 +0330

    feat(launch-editor-middleware): add connect support (#1)

--- packages/launch-editor-middleware/index.js
+++ packages/launch-editor-middleware/index.js
@@ -1,10 +1,12 @@
+const url = require('url')
 const launch = require('launch-editor')
 
 module.exports = (specifiedEditor, onErrorCallback) => {
   return function launchEditorMiddleware (req, res, next) {
-    const { file } = req.query
+    const { file } = url.parse(req.url, true).query || {}
     if (!file) {
-      res.status(500).end(`launch-editor-middleware: required query param "file" is missing.`)
+      res.statusCode = 500
+      res.end(`launch-editor-middleware: required query param "file" is missing.`)
     } else {
       launch(file, specifiedEditor, onErrorCallback)
       res.end()
