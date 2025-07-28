dd60ac6cb19eaad03fda19d82cf3972f068ba807
Haoqun Jiang <haoqunjiang@gmail.com>
2025-07-28 15:30:12 +0800

    style: continue formatting

--- packages/launch-editor-middleware/index.js
+++ packages/launch-editor-middleware/index.js
@@ -14,22 +14,24 @@ module.exports = (specifiedEditor, srcRoot, onErrorCallback) => {
 
   srcRoot = srcRoot || process.cwd()
 
-  return function launchEditorMiddleware (req, res) {
-    let url;
+  return function launchEditorMiddleware(req, res) {
+    let url
 
     try {
-      url = new URL(req.url);
-    // eslint-disable-next-line no-unused-vars
+      url = new URL(req.url)
+      // eslint-disable-next-line no-unused-vars
     } catch (_err) {
-      res.statusCode = 500;
+      res.statusCode = 500
       res.end(`launch-editor-middleware: invalid URL.`)
-      return;
+      return
     }
 
-    const file = url.searchParams.get("file");
+    const file = url.searchParams.get('file')
     if (!file) {
       res.statusCode = 500
-      res.end(`launch-editor-middleware: required query param "file" is missing.`)
+      res.end(
+        `launch-editor-middleware: required query param "file" is missing.`
+      )
     } else {
       launch(path.resolve(srcRoot, file), specifiedEditor, onErrorCallback)
       res.end()
